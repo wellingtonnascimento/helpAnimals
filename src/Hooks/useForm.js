b03fe7f59402aa0e820d0251d73777f98c50e90b
Wellington Nascimento <wellingto_07@hotmail.com>
2021-07-27 21:14:43 -0300

    create hook useForm

---
+++ src/Hooks/useForm.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+const types = {
+  email: {
+    regex:
+      /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/gi,
+    message: "Preencha um email valido",
+  },
+};
+
+const useForm = (type) => {
+  const [value, setValue] = useState("");
+  const [error, setError] = useState(null);
+
+  function validate(value) {
+    if (type === false) return true;
+    if (value.length === 0) {
+      setError("Preencha um valor");
+      return false;
+    } else if (types[type] && !types[type].regex.test(value)) {
+      setError(types[type].message);
+      return false;
+    } else {
+      setError(null);
+      return true;
+    }
+  }
+  function onChange({ target }) {
+    if (error) validate(target.value);
+    setValue(target.value);
+  }
+
+  return {
+    value,
+    setValue,
+    onChange,
+    error,
+    validate: () => validate(value),
+    onBlur: () => validate(value),
+  };
+};
+
+export default useForm;
