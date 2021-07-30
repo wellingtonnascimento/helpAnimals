df508e5abb8d83480d8fe5e6dbe71cdba15bc441
Wellington Nascimento <wellingto_07@hotmail.com>
2021-07-30 17:05:02 -0300

    validation token

--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -1,11 +1,15 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+import React, { useContext } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { UserContext } from "../../Contexts/UserContext";
 import LoginCreate from "./LoginCreate";
 import LoginForm from "./LoginForm";
 import LoginPasswordLost from "./LoginPasswordLost";
 import LoginPasswordReset from "./LoginPasswordReset";
 
 const Login = () => {
+  const { login } = useContext(UserContext);
+
+  if (login === true) return <Navigate to="/conta" />;
   return (
     <div>
       <Routes>
