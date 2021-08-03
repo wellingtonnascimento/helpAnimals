ada9ad6fa21a76d92cd84b6091d343ff81c5ebc6
Wellington Nascimento <wellingto_07@hotmail.com>
2021-08-03 20:33:40 -0300

    create hook use fatch

--- src/Components/Login/LoginCreate.js
+++ src/Components/Login/LoginCreate.js
@@ -3,7 +3,9 @@ import useForm from "../../Hooks/useForm";
 import { USER_POST } from "../../service/api";
 import { UserContext } from "../../Contexts/UserContext";
 import Button from "../Forms/Button";
+import Error from "../Helper/Error";
 import Input from "../Forms/Input";
+import useFatch from "../../Hooks/useFatch";
 
 const LoginCreate = () => {
   const username = useForm();
@@ -11,6 +13,7 @@ const LoginCreate = () => {
   const password = useForm("password");
 
   const { userLogin } = useContext(UserContext);
+  const { loading, error, request } = useFatch();
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -19,7 +22,7 @@ const LoginCreate = () => {
       email: email.value,
       password: password.value,
     });
-    const response = await fetch(url, options);
+    const { response } = await request(url, options);
     if (response.ok) userLogin(username.value, password.value);
   }
 
@@ -30,7 +33,13 @@ const LoginCreate = () => {
         <Input label="Usuário" type="text" name="username" {...username} />
         <Input label="Email" type="email" name="email" {...email} />
         <Input label="Senha" type="password" name="pawword" {...password} />
-        <Button>Cadastrar</Button>
+        {loading ? (
+          <Button disabled>Cadastrando...</Button>
+        ) : (
+          <Button>Cadastrar</Button>
+        )}
+
+        <Error error={error} />
       </form>
     </section>
   );
---
+++ src/Hooks/useFatch.js
@@ -0,0 +1,37 @@
+import React, { useCallback, useState } from "react";
+
+const useFatch = () => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const request = useCallback(async (url, options) => {
+    let response;
+    let json;
+
+    try {
+      setError(null);
+      setLoading(true);
+      response = await fetch(url, options);
+      json = await response.json();
+      if (response.ok === false) throw new Error(json.message);
+    } catch (err) {
+      json = null;
+      setError(err.message);
+    } finally {
+      setData(json);
+      setLoading(false);
+
+      return { response, json };
+    }
+  }, []);
+
+  return {
+    data,
+    loading,
+    error,
+    request,
+  };
+};
+
+export default useFatch;
