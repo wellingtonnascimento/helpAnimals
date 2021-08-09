0b276ffc6675e4d5a59bb3545c50efeadd93b72f
Wellington Nascimento <wellingto_07@hotmail.com>
2021-08-09 20:46:30 -0300

    create feed

--- src/Components/Feed/index.js
+++ src/Components/Feed/index.js
@@ -1,7 +1,14 @@
 import React from "react";
+import FeedModal from "./FeedModal";
+import FeedPhotos from "./FeedPhotos";
 
 const Feed = () => {
-  return <div>fff</div>;
+  return (
+    <div>
+      <FeedModal />
+      <FeedPhotos />
+    </div>
+  );
 };
 
 export default Feed;
--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Feed from "../Feed";
 
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <section className="container mainContainer">
+      <Feed />
+    </section>
+  );
 };
 
 export default Home;
