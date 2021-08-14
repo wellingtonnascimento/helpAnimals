84c13ff725e8bb9162ca3635f277366ad969f5bc
Wellington Nascimento <wellingto_07@hotmail.com>
2021-08-13 23:22:36 -0300

    create componets delete image

--- src/Components/Photo/PhotoContent/PhotoContent.js
+++ src/Components/Photo/PhotoContent/PhotoContent.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import styles from "./PhotoContent.module.css";
 import { Link } from "react-router-dom";
 import PhotoComments from "../PhotoComments/PhotoComments";
+import { UserContext } from "../../../Contexts/UserContext";
+import PhotoDelete from "../PhotoDelete/PhotoDelete";
 
 const PhotoContent = ({ data }) => {
+  const user = useContext(UserContext);
   const { photo, comments } = data;
+
   return (
     <div className={styles.photo}>
       <div className={styles.img}>
@@ -13,7 +17,11 @@ const PhotoContent = ({ data }) => {
       <div className={styles.details}>
         <div>
           <p className={styles.author}>
-            <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            {user.data && user.data.username === photo.author ? (
+              <PhotoDelete id={photo.id} />
+            ) : (
+              <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            )}
             <span className={styles.visualizacoes}>{photo.acessos}</span>
           </p>
           <h1 className="title">
