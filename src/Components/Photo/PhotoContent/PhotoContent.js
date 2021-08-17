d129853a8e83572f8338f50434e07eb0bd5908c6
Wellington Nascimento <wellingto_07@hotmail.com>
2021-08-17 20:49:59 -0300

    image skeleton, delete img

--- src/Components/Photo/PhotoContent/PhotoContent.js
+++ src/Components/Photo/PhotoContent/PhotoContent.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import PhotoComments from "../PhotoComments/PhotoComments";
 import { UserContext } from "../../../Contexts/UserContext";
 import PhotoDelete from "../PhotoDelete/PhotoDelete";
+import Image from "../../Helper/Image";
 
 const PhotoContent = ({ data }) => {
   const user = useContext(UserContext);
@@ -12,7 +13,7 @@ const PhotoContent = ({ data }) => {
   return (
     <div className={styles.photo}>
       <div className={styles.img}>
-        <img src={photo.src} alt={photo.title} />
+        <Image src={photo.src} alt={photo.title} />
       </div>
       <div className={styles.details}>
         <div>
