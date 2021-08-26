a957347af627c59037f4a36e1b4cf9d11bfc42f6
Wellington Nascimento <wellingto_07@hotmail.com>
2021-08-25 21:52:20 -0300

    implement, pie chart victory

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
+import useFetch from "../../Hooks/useFetch";
+import { STATS_GET } from "../../service/api";
+import Error from "../Helper/Error";
+import Head from "../Helper/Head";
+import Loading from "../Helper/Loading";
+import UserStatsGraphs from "./UserStatsGraphs";
 
 const UserStats = () => {
-  return <div>gg</div>;
+  const { data, error, loading, request } = useFetch();
+
+  useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET();
+      await request(url, options);
+    }
+    getData();
+  }, [request]);
+
+  console.log(data);
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (data)
+    return (
+      <div>
+        <Head title="Estatísticas" />
+        <UserStatsGraphs data={data} />
+      </div>
+    );
+  else return null;
 };
 
 export default UserStats;
