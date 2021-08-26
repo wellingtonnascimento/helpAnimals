d0906ffd6c2987db4285f7826ed991a34776d02d
Wellington Nascimento <wellingto_07@hotmail.com>
2021-08-25 21:19:15 -0300

    create component head page

--- src/Components/User/UserProfile.js
+++ src/Components/User/UserProfile.js
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import Feed from "../Feed";
+import Head from "../Helper/Head";
 
 const UserProfile = () => {
   const { user } = useParams();
@@ -7,6 +8,7 @@ const UserProfile = () => {
   return (
     <section className="container mainSection">
       <h1 className="title">{user}</h1>
+      <Head title={user} />
       <Feed user={user} />
     </section>
   );
