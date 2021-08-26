58ab8b432d3542b0d29d52ea1da537051f06410a
Wellington Nascimento <wellingto_07@hotmail.com>
2021-08-25 21:26:45 -0300

    create footer

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import styles from "./Footer.module.css";
+import { ReactComponent as Dogs } from "../../Assets/dogs-footer.svg";
 
 const Footer = () => {
-  return <div className={styles.Footer}></div>;
+  return (
+    <div className={styles.footer}>
+      <Dogs />
+      <p>Help Animals. Alguns direitos reservados.</p>
+    </div>
+  );
 };
 
 export default Footer;
